06afaa2425e854366ef992f0adc28be074edd2fa
Niclas Hedegaard Jensen <4251729@RTS-365.dk>
2020-06-16 14:08:25 +0200

    add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,41 @@
-let toggleButton = document.querySelector('.toggle-menu');
-let navBar = document.querySelector('.nav-bar');
-toggleButton.addEventListener('click', function () {
-	navBar.classList.toggle('toggle');
+let mainNav = document.getElementById('uList');
+let navBarToggle = document.querySelector("#checkboxLabel");
+let checbox = document.querySelector("#checkbox");
+
+
+// Checkbox unchecked when site width is over 767px
+  var mq = window.matchMedia( "(max-width: 767px)" );
+  if (mq.matches) {}
+  else {
+    checbox.checked = false
+  }
+
+// Give class active when you click on the Burger Menu
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle('active');
 });
+
+// Dropdown JAMSTACK, give flex-style when pressed and remove when pressed again.
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("activeJob");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "flex") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "flex";
+    }
+  });
+}
+
+let dropdwn = document.querySelector(".dropdown-container");
+
+$(".gallery").magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+  });
